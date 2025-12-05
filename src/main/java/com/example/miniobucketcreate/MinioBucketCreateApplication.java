51059cfad13475775d8942c6dd261779b7ea2d78
patoshim3 <patimaactposhta@gmail.com>
2025-12-05 14:17:50 +0500

    First commit init

---
+++ src/main/java/com/example/miniobucketcreate/MinioBucketCreateApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniobucketcreate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinioBucketCreateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MinioBucketCreateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/miniobucketcreate/MinioBucketCreateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniobucketcreate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinioBucketCreateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
